71c9dbc8a19338c7b82ddfdd26d13e814fba6b83
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-04-28 15:52:55 +0530

    [VINAY] Add: Displaying Json File Data with the Helep of the Serializatin

---
+++ InventoryManagementWithJson/InventoryDataManagement/InventoryDataDisplay.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementWithJson.InventoryDataManagement
+{
+    public class InventoryDataDisplay
+    {
+        public void DisplayInventoryData(string filePath)
+        {
+            StreamReader reader = new StreamReader(filePath);
+
+            string json = reader.ReadToEnd();
+            var inventory = JsonConvert.DeserializeObject<List<InventoryData>>(json);
+            Console.WriteLine("Name" + "\t" + "PricePrKg" + "\t" + "Weight" + "\t\t" + "TotalPrice");
+            foreach (InventoryData item in inventory)
+            {
+                Console.WriteLine(item.Name + "\t" + item.Price + "\t\t" + item.Weight + "\t\t" + (item.Price * item.Weight));
+            }
+        }
+    }
+}
---
+++ InventoryManagementWithJson/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using InventoryManagementWithJson;
+using InventoryManagementWithJson.InventoryDataManagement;
+
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        Console.WriteLine(" ***** Welcome In The Inventory Management ***** ");
+        const string INVENTORY_FILE_PATH = @"D:\InventoryManagementSystem\InventoryManagement\InventoryManagementWithJson\InventoryDataManagement\Inventory.json";
+        InventoryDataDisplay inventoryData = new InventoryDataDisplay();
+        Console.WriteLine(" Display 1\n");
+        bool check = true;
+        while (check)
+        {
+            Console.WriteLine("Enter the Above Option");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    inventoryData.DisplayInventoryData(INVENTORY_FILE_PATH);
+                    break;
+                case 0:
+                    check = false;
+                    break;
+            }
+        }
+    }
+}
